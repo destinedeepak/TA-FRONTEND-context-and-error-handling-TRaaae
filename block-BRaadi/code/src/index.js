cca2b1c55b897a1314cf94540aa91036128aaa8b
Deepak Singh <deepaksingh@Deepaks-MacBook-Air.local>
2021-10-12 18:29:18 +0530

    ac

--- block-BRaadi/code/src/index.js
+++ block-BRaadi/code/src/index.js
@@ -1,11 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./style/app.css";
-import App from "./components/App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style/app.css';
+import App from './components/App';
+import ErrorBoundary from './components/ErrorBoundary';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  <ErrorBoundary>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ErrorBoundary>,
+  document.getElementById('root')
 );
